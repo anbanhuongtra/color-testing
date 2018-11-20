cc88b10b3a2f007359facf342a76e6a521a2eadb
Minh Tri <37803576+anbanhuongtra@users.noreply.github.com>
2018-11-20 16:22:35 +0700

    da
    dzv

---
+++ GameChonMau/GameChonMau/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace GameChonMau
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbPoint = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lbMiss = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbTime = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(957, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 38);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "ĐIỂM";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(451, 39);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(400, 369);
+            this.flowLayoutPanel1.TabIndex = 11;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(974, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 38);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "LỖI";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.Controls.Add(this.lbPoint);
+            this.panel1.Location = new System.Drawing.Point(951, 124);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(122, 57);
+            this.panel1.TabIndex = 16;
+            // 
+            // lbPoint
+            // 
+            this.lbPoint.AutoSize = true;
+            this.lbPoint.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPoint.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbPoint.Location = new System.Drawing.Point(42, 12);
+            this.lbPoint.Name = "lbPoint";
+            this.lbPoint.Size = new System.Drawing.Size(35, 38);
+            this.lbPoint.TabIndex = 13;
+            this.lbPoint.Text = "0";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Firebrick;
+            this.panel2.Controls.Add(this.lbMiss);
+            this.panel2.Location = new System.Drawing.Point(951, 262);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(122, 57);
+            this.panel2.TabIndex = 17;
+            // 
+            // lbMiss
+            // 
+            this.lbMiss.AutoSize = true;
+            this.lbMiss.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMiss.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbMiss.Location = new System.Drawing.Point(42, 12);
+            this.lbMiss.Name = "lbMiss";
+            this.lbMiss.Size = new System.Drawing.Size(35, 38);
+            this.lbMiss.TabIndex = 13;
+            this.lbMiss.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTime.ForeColor = System.Drawing.Color.Coral;
+            this.lbTime.Location = new System.Drawing.Point(351, 53);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(55, 39);
+            this.lbTime.TabIndex = 18;
+            this.lbTime.Text = "15";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 444);
+            this.Controls.Add(this.lbTime);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbPoint;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lbMiss;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbTime;
+    }
+}
+
---
+++ GameChonMau/GameChonMau/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameChonMau
+{
+    public partial class Form1 : Form
+    {
+      
+        int point = 0;
+        int miss = 0;
+        int time = 15;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            RenderGrid();
+        }
+
+       
+        
+
+        public int GetGridLevel(int level)
+        {
+            if (level < 2) return 2;
+            if (level < 4) return 3;
+            if (level < 8) return 4;
+            if (level < 13) return 5;
+            if (level < 22) return 6;
+            if (level < 32) return 7;
+            if (level < 36) return 8;
+            if (level < 40) return 9;
+            if (level < 44) return 10;
+            if (level < 48) return 11;
+            return 12;
+        }
+        int level = 1;
+        int correct_tile = -1;
+
+        Random color_random = new Random();
+        Random tile_random = new Random();
+
+        public void RenderGrid()
+        {
+            int grid = GetGridLevel(level);
+
+            //set position of correct tile
+            int position = -1;
+            do
+            {
+                position = tile_random.Next(0, grid * grid -1);
+            } while (correct_tile == position);
+
+            //set color 
+            var colordiff = getLevelColorDiff(level);
+            var r = color_random.Next(0, (255 - colordiff));
+            var g = color_random.Next(0, (255 - colordiff));
+            var b = color_random.Next(0, (255 - colordiff));
+
+            //render grid
+            for (int i = 0; i < grid * grid; i++)
+            {
+                Button tile = new Button();
+                tile.Width = (flowLayoutPanel1.Width / grid) - 10; // error at higher level
+                tile.Height = (flowLayoutPanel1.Height / grid) - 10; // error at higher level
+                tile.Tag = i;
+                
+                if (i != position)
+                {
+                    tile.BackColor = Color.FromArgb(255, r, g, b);
+                    tile.Click += tile_Click_miss;
+                }
+                else
+                {
+                    tile.BackColor = Color.FromArgb(255, r+colordiff, g+colordiff, b+colordiff);
+                    tile.Click += tile_Click_correct;
+                }
+                flowLayoutPanel1.Controls.Add(tile);
+            }
+
+            
+
+
+        }
+
+
+      
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        //tile click event
+        void tile_Click_correct(object sender, EventArgs e)
+        {
+            
+            Button btn = sender as Button;
+            level++;
+            flowLayoutPanel1.Controls.Clear();
+            RenderGrid();
+            point++;
+            lbPoint.Text = point.ToString();
+            time = 16;
+            timer1.Start();
+            
+
+        }
+
+        void tile_Click_miss(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            miss++;
+            lbMiss.Text = miss.ToString();
+            time -= 3;
+
+        }
+
+
+        //  set difficult level
+        int[] col = { 105, 75, 60, 45, 30, 20, 18, 16, 15, 15, 15, 14, 14, 14, 13, 13, 13, 12, 12, 12, 11, 11, 11, 10, 10, 9, 9, 8, 8, 7, 7, 7, 7, 6, 6, 6, 6, 5, 5, 5, 5, 4, 4, 4, 4, 3, 3, 3, 3, 2, 2, 2, 2, 1, 1, 1, 1, 1 };
+        public int getLevelColorDiff(int level)
+        {
+            if (level <= 58)
+            {
+                return col[level - 1];
+            }
+            return 1;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time--;
+            lbTime.Text = time.ToString();
+
+            if (time <= 0) { timer1.Stop(); lbTime.Text = "0"; }
+
+        }
+    }
+}
